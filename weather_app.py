c7ebc9478a6084c4668ef74c3e8cc3588b203c0a
skuppili-lab <skuppili@student.gitam.edu>
2026-06-14 17:22:51 +0530

    updated

--- weather_app.py
+++ weather_app.py
@@ -2,7 +2,7 @@ import requests
 city=input("enter your city: ")
 url=f"https://wttr.in/{city}?format=j1"    
 response = requests.get(url).json()
-obv_time = response["current_condition"][0]["observation_time"][0]
+obv_time = response["current_condition"][0]["observation_time"]
 temp = response["current_condition"][0]["temp_C"]
 weather = response["current_condition"][0]["weatherDesc"][0]["value"]
 humidity = response["current_condition"][0]["humidity"]
